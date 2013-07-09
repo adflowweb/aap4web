2b471f2987a773b660d6e021e1e01631e883c332
typark <typark@adflow.co.kr>
2013-07-09 16:47:32 +0900

    modified by nadir

--- config.js
+++ config.js
@@ -5,3 +5,12 @@
  * Time: 오후 3:47
  * To change this template use File | Settings | File Templates.
  */
+module.exports = {
+    "db": {
+        "mongodb": "mongodb://username:password@dsXXXXX.mongolab.com:45077/databasename"
+    },
+    "logger": {
+        "api": "logs/api.log",
+        "exception": "logs/exceptions.log"
+    }
+};
