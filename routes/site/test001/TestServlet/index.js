5ede1bf93bcc36cd1c123e573871f562bfba2c95
typark <typark@adflow.co.kr>
2013-07-10 11:23:28 +0900

    hex encoding을 urlencoding으로 변경

--- routes/site/test001/TestServlet/index.js
+++ routes/site/test001/TestServlet/index.js
@@ -39,7 +39,13 @@ exports.put = function (req, res, client) {
         $("#dynamicTable").append("</tbody>");
 
         //console.log('<html>' + $('html').html() + '</html>');
-        client.set(req.params.id, '<html>' + $('html').html() + '</html>', redis.print);
+        try {
+            client.set(req.params.id, '<html>' + $('html').html() + '</html>', client.print);
+        }
+        catch (err) {
+            console.log('err', err);
+        }
+
 
         var responseData = $('html').html().replace(/[\n\r]/g, '').replace(/\s+/g, '');
         console.log('response data : ', responseData);
@@ -47,17 +53,19 @@ exports.put = function (req, res, client) {
         //var key = 'abcdeg'
 
         //console.log('hash : ', crypto.createHash('sha1').update('this is test').digest('hex'));
-        var bytes = Buffer.byteLength(responseData, 'utf8');
-        var hex = '';
-        console.log(responseData.length + " characters, " + bytes + " bytes");
-        for (var i = 0; i < bytes; i++) {
-            hex += responseData.charCodeAt(i).toString(16);
-            //console.log('hex : ', hex);
-        }
-        console.log('encoded string : ', hex);
+        //var bytes = Buffer.byteLength(responseData, 'utf8');
+        //var hex = '';
+        //console.log(responseData.length + " characters, " + bytes + " bytes");
+        //for (var i = 0; i < bytes; i++) {
+        //    hex += responseData.charCodeAt(i).toString(16);
+        //    //console.log('hex : ', hex);
+        //}
+        var enc = encodeURIComponent(responseData);
+
+        console.log('encoded string : ', enc);
 
         //console.log('hash : ', crypto.createHmac('sha1', key).update('<html>'+$('html').html()+'</html>').digest('hex'));
-        console.log('hash : ', crypto.createHash('sha1').update(hex).digest('hex'));
+        console.log('hash : ', crypto.createHash('sha1').update(enc).digest('hex'));
     });
     res.end('ok');
 }
