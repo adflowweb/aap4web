a6d0fddc682478bbc862596ecf1473bda82dbe10
typark <typark@adflow.co.kr>
2013-07-11 16:32:17 +0900

    crud test 완성

--- test/test.js
+++ test/test.js
@@ -13,7 +13,7 @@ var winston = require('winston');
 var config = require('../config');
 var url = 'http://127.0.0.1:3000';
 
-describe('Routing', function() {
+describe('Routing', function () {
     // within before() you can run all the operations that are needed to setup your tests. In this case
     // I want to create a connection with the database, and when I'm done, I call done().
     before(function (done) {
@@ -50,7 +50,7 @@ describe('Routing', function() {
                 });
         });
 
-        it('should return code 200 trying to create virtualpage', function (done) {
+        it('should return code 200 trying to modify virtualpage', function (done) {
             // once we have specified the info we want to send to the server via POST verb,
             // we need to actually perform the action on the resource, in this case we want to
             // POST on /api/profiles and we want to send some info
@@ -69,6 +69,43 @@ describe('Routing', function() {
                     done();
                 });
         });
+
+        it('should return code 200 trying to get virtualpage', function (done) {
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to
+            // POST on /api/profiles and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .get('/v1/virtualpages/1234567890')
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+
+        it('should return code 200 trying to delete virtualpage', function (done) {
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to
+            // POST on /api/profiles and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .del('/v1/virtualpages/1234567890')
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+
 //        it('should correctly update an existing account', function(done){
 //            var body = {
 //                firstName: 'JP',
