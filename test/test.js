72badef875bc89ccbb1da1af12cdb45d031899b9
typark <typark@adflow.co.kr>
2013-07-11 15:35:28 +0900

    testCode 수정

--- test/test.js
+++ test/test.js
@@ -12,18 +12,18 @@ var request = require('supertest');
 var winston = require('winston');
 var config = require('../config');
 
-describe('Routing', function() {
+describe('Routing', function () {
     var url = 'http://127.0.0.1:3000';
     // within before() you can run all the operations that are needed to setup your tests. In this case
     // I want to create a connection with the database, and when I'm done, I call done().
-    before(function(done) {
+    before(function (done) {
         // In our tests we use the test db
         //mongoose.connect(config.db.mongodb);
         done();
     });
 
-    describe('virtualpages', function() {
-        it('should return code 200 trying to create virtualpage', function(done) {
+    describe('virtualpages', function () {
+        it('should return code 200 trying to create virtualpage', function (done) {
             var profile = {
                 username: 'vgheri',
                 password: 'test',
@@ -36,11 +36,31 @@ describe('Routing', function() {
             // We do this using the request object, requiring supertest!
             request(url)
                 .post('/v1/virtualpages/1234567890')
-                .set('request_uri_origin','/test001/index.jsp')
+                .set('request_uri_origin', '/test001/index.jsp')
                 .attach('nadir', './test/test.html')
                 //.send(profile)
                 // end handles the response
-                .end(function(err, res) {
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+
+        it('should return code 200 trying to create virtualpage', function (done) {
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to
+            // POST on /api/profiles and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .put('/v1/virtualpages/1234567890')
+                .set('request_uri_origin', '/test001/TestServlet')
+                //.send(profile)
+                // end handles the response
+                .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
