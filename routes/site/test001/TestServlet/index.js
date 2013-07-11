71533b88b232201dc2a0738a3e65ae382a5ef3cc
typark <typark@adflow.co.kr>
2013-07-11 15:40:28 +0900

    redis관련 작업을 virtualPageHandler가 전담하므로 페이지에서는 순수 javascript관련 작업에만 신경쓰면됨

--- routes/site/test001/TestServlet/index.js
+++ routes/site/test001/TestServlet/index.js
@@ -8,64 +8,30 @@
 var parser = require('cheerio'),
     crypto = require('crypto');
 
-exports.put = function (req, res, client) {
+exports.put = function (req, res, data) {
     console.log('req.headers : ', req.headers);
     console.log('----------------------------------------------');
+    var $ = parser.load(data);
 
-    client.get(req.params.id, function (err, reply) {
-        // reply is null when the key is missing
-        //console.log('reply : ', reply);
-        var $ = parser.load(reply);
+    $("#msg").text("Validation Success");
+    $("#dynamicTable").append("<caption>" + "ChanTable" + "</caption>");
+    $("#dynamicTable").append("<tbody>");
+    $("#dynamicTable").append("<tr>");
+    $("#dynamicTable").append("<td>A</td>");
+    $("#dynamicTable").append("<td>B</td>");
+    $("#dynamicTable").append("<td>C</td>");
+    $("#dynamicTable").append("</tr>");
+    $("#dynamicTable").append("<tr>");
+    $("#dynamicTable").append("<td>D</td>");
+    $("#dynamicTable").append("<td>E</td>");
+    $("#dynamicTable").append("<td>F</td>");
+    $("#dynamicTable").append("</tr>");
+    $("#dynamicTable").append("<tr>");
+    $("#dynamicTable").append("<td>G</td>");
+    $("#dynamicTable").append("<td>H</td>");
+    $("#dynamicTable").append("<td>I</td>");
+    $("#dynamicTable").append("</tr>");
+    $("#dynamicTable").append("</tbody>");
 
-        $("#msg").text("Validation Success");
-
-        $("#dynamicTable").append("<caption>" + "ChanTable" + "</caption>");
-        $("#dynamicTable").append("<tbody>");
-        $("#dynamicTable").append("<tr>");
-        $("#dynamicTable").append("<td>A</td>");
-        $("#dynamicTable").append("<td>B</td>");
-        $("#dynamicTable").append("<td>C</td>");
-        $("#dynamicTable").append("</tr>");
-        $("#dynamicTable").append("<tr>");
-        $("#dynamicTable").append("<td>D</td>");
-        $("#dynamicTable").append("<td>E</td>");
-        $("#dynamicTable").append("<td>F</td>");
-        $("#dynamicTable").append("</tr>");
-        $("#dynamicTable").append("<tr>");
-        $("#dynamicTable").append("<td>G</td>");
-        $("#dynamicTable").append("<td>H</td>");
-        $("#dynamicTable").append("<td>I</td>");
-        $("#dynamicTable").append("</tr>");
-        $("#dynamicTable").append("</tbody>");
-
-        //console.log('<html>' + $('html').html() + '</html>');
-        try {
-            client.set(req.params.id, '<html>' + $('html').html() + '</html>', client.print);
-        }
-        catch (err) {
-            console.log('err', err);
-        }
-
-
-        var responseData = $('html').html().replace(/[\n\r]/g, '').replace(/\s+/g, '');
-        console.log('response data : ', responseData);
-        console.log('----------------------------------------------');
-        //var key = 'abcdeg'
-
-        //console.log('hash : ', crypto.createHash('sha1').update('this is test').digest('hex'));
-        //var bytes = Buffer.byteLength(responseData, 'utf8');
-        //var hex = '';
-        //console.log(responseData.length + " characters, " + bytes + " bytes");
-        //for (var i = 0; i < bytes; i++) {
-        //    hex += responseData.charCodeAt(i).toString(16);
-        //    //console.log('hex : ', hex);
-        //}
-        var enc = encodeURIComponent(responseData);
-
-        console.log('encoded string : ', enc);
-
-        //console.log('hash : ', crypto.createHmac('sha1', key).update('<html>'+$('html').html()+'</html>').digest('hex'));
-        console.log('hash : ', crypto.createHash('sha1').update(enc).digest('hex'));
-    });
-    res.end('ok');
+    return  '<html>' + $('html').html() + '</html>';
 }
