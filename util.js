a513f6568a36d5273f194650d945b5bbef324241
nadir93 <nadir93@gmail.com>
2013-08-08 16:55:28 +0900

    성능테스트용 logging level error로 변경

--- util.js
+++ util.js
@@ -8,14 +8,14 @@ var parser = require('cheerio'),
     logger = require('./logger');
 
 exports.normalize = function (data) {
-    //logger.debug(__filename + ' data : ', data);
+    logger.debug(__filename + ' before data : ', data);
     var normalizedData = encodeURIComponent(data.replace(/[\n\r]/g, '').replace(/\s+/g, ''));
-    //logger.debug(__filename + ' normalizedData : ', normalizedData);
+    logger.debug(__filename + ' normalizedData : ', normalizedData);
     return normalizedData;
 };
 
 exports.hash = function (data) {
-    //logger.debug(__filename + ' data : ', data);
+    logger.debug(__filename + ' before data : ', data);
     var hash = crypto.createHash('sha256').update(data).digest('hex');
     logger.debug(__filename + ' hash : ', hash);
     //res.end('<html>'+$('html').html()+'</html>');
