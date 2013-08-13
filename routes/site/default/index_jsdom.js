5a2cd3d4deaf335090efc07207f93633532da11a
nadir93 <nadir93@gmail.com>
2013-08-13 11:22:32 +0900

    jsdom test code 변경

--- routes/site/default/index_jsdom.js
+++ routes/site/default/index_jsdom.js
@@ -12,20 +12,25 @@ exports.post = function (req, res) {
     //logger.debug('req.rawBody : ', req.rawBody);
 
     var window = jsdom.jsdom(req.rawBody).createWindow();
-    jsdom.jQueryify(window, function(){
-        logger.debug(__filename + ' jquery version : ', window.$().jquery);
-        // console.log(window.document.innerHTML);
-        logger.debug(__filename + ' jsdom : ', window.$('html').html());
-        // console.log(window.location);
-        //res.end("<html>"+window.$('html').html()+"</html>")
-        if (window.$('html').html()) {
-            return window.$('html').html();
-        }
-        else {
-            //return '<HTML>' + $('HTML').html() + '</HTML>';
-            return window.$('HTML').html();
-        }
-    });
+    logger.debug(__filename + ' jsdom : ', window.document.documentElement.innerHTML);
+    return window.document.documentElement.innerHTML
+
+
+
+//    jsdom.jQueryify(window, function(){
+//        logger.debug(__filename + ' jquery version : ', window.$().jquery);
+//        // console.log(window.document.innerHTML);
+//        logger.debug(__filename + ' jsdom : ', window.$('html').html());
+//        // console.log(window.location);
+//        //res.end("<html>"+window.$('html').html()+"</html>")
+//        if (window.$('html').html()) {
+//            return window.$('html').html();
+//        }
+//        else {
+//            //return '<HTML>' + $('HTML').html() + '</HTML>';
+//            return window.$('HTML').html();
+//        }
+//    });
 
 
 };
