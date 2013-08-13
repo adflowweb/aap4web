c44e1cb26bcb4d1df01205a952f44877905c66f4
nadir93 <nadir93@gmail.com>
2013-08-13 13:42:31 +0900

    html 정규화 수정.

--- routes/site/default/index_jsdom.js
+++ routes/site/default/index_jsdom.js
@@ -3,34 +3,43 @@
  * Date: 13. 8. 13.
  * Time: 오전 10:31
  */
-var jsdom = require('jsdom'),
+var jsdom = require('jsdom').jsdom,
     logger = require('../../../logger');
 
 exports.post = function (req, res) {
-    logger.debug(__filename + ' called post default/index_jsdom.js');
-    //logger.debug('req.xhr : ', req.xhr);
-    //logger.debug('req.rawBody : ', req.rawBody);
 
-    var window = jsdom.jsdom(req.rawBody).createWindow();
-    //logger.debug(__filename + ' jsdom : ', window.document.documentElement.innerHTML);
-    //return window.document.documentElement.innerHTML
+    var document = jsdom(req.rawBody);
+    var window = document.parentWindow;
+
+    logger.debug(__filename + ' jsdom : ', window.document.documentElement.innerHTML);
+
+    return window.document.documentElement.innerHTML;
 
 
-
-    jsdom.jQueryify(window, function(){
-        logger.debug(__filename + ' jquery version : ', window.$().jquery);
-        // console.log(window.document.innerHTML);
-        logger.debug(__filename + ' jsdom : ', window.$('html').html());
-        // console.log(window.location);
-        //res.end("<html>"+window.$('html').html()+"</html>")
-        if (window.$('html').html()) {
-            return window.$('html').html();
-        }
-        else {
-            //return '<HTML>' + $('HTML').html() + '</HTML>';
-            return window.$('HTML').html();
-        }
-    });
+//    logger.debug(__filename + ' called post default/index_jsdom.js');
+//    //logger.debug('req.xhr : ', req.xhr);
+//    //logger.debug('req.rawBody : ', req.rawBody);
+//
+//    var window = jsdom.jsdom(req.rawBody).createWindow();
+//    //logger.debug(__filename + ' jsdom : ', window.document.documentElement.innerHTML);
+//    //return window.document.documentElement.innerHTML
+//
+//
+//
+//    jsdom.jQueryify(window, function(){
+//        logger.debug(__filename + ' jquery version : ', window.$().jquery);
+//        // console.log(window.document.innerHTML);
+//        logger.debug(__filename + ' jsdom : ', window.$('html').html());
+//        // console.log(window.location);
+//        //res.end("<html>"+window.$('html').html()+"</html>")
+//        if (window.$('html').html()) {
+//            return window.$('html').html();
+//        }
+//        else {
+//            //return '<HTML>' + $('HTML').html() + '</HTML>';
+//            return window.$('HTML').html();
+//        }
+//    });
 
 
 };
