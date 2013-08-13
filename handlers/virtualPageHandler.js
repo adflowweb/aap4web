18edf4472f3a3d1a4e0c758c04b30aa5ddeaa7bf
nadir93 <nadir93@gmail.com>
2013-08-13 13:23:00 +0900

    html 정규화 수정.

--- handlers/virtualPageHandler.js
+++ handlers/virtualPageHandler.js
@@ -5,7 +5,7 @@
  */
 var util = require('util'),
     logger = require('../logger'),
-    DEFAULT_INDEX_JS = '../routes/site/default/index_jsdon.js',
+    DEFAULT_INDEX_JS = '../routes/site/default/index_jsdom.js',
     RESPONSE_MESSAGE = '{"error":{"code":400,"message":"virtual_page_uri header not found"}}';
 
 var virtualPageHandler = function () {
