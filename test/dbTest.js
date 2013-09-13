6cfc8e130b509222884fbc59a968789a78c9c56c
nadir93 <nadir93@gmail.com>
2013-09-13 12:51:52 +0900

    oracle test

--- test/dbTest.js
+++ test/dbTest.js
@@ -6,8 +6,14 @@
 var oracle = require("oracle");
 
 var connectData = { "hostname": "192.168.1.39", "user": "aap4web", "password": "aap4web1234", "database": "orcl" };
-
+console.log('connecting : ',connectData);
 oracle.connect(connectData, function (err, connection) {
-    console.log(util.inspector(connection));
+    if(err)
+    {
+        console.log('err : ', err);
+        return;
+    }
+
+    console.log('conn : ', connection);
     connection.close(); // call this when you are done with the connection
 });
