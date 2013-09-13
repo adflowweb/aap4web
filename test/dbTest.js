90624fee35da2d6503c6a37fab2acdf5e8948c4e
nadir93 <nadir93@gmail.com>
2013-09-13 12:36:40 +0900

    oracle test

---
+++ test/dbTest.js
@@ -0,0 +1,13 @@
+/**
+ * User: nadir93
+ * Date: 13. 9. 13.
+ * Time: 오전 10:39
+ */
+var oracle = require("oracle");
+
+var connectData = { "hostname": "192.168.1.39", "user": "aap4web", "password": "aap4web1234", "database": "orcl" };
+
+oracle.connect(connectData, function (err, connection) {
+    console.log(util.inspector(connection));
+    connection.close(); // call this when you are done with the connection
+});
