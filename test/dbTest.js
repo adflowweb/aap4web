246ed575226e9106db8b941d0cdef6eb9daf8294
nadir93 <nadir93@gmail.com>
2013-09-13 14:33:07 +0900

    oracle test

--- test/dbTest.js
+++ test/dbTest.js
@@ -3,28 +3,72 @@
  * Date: 13. 9. 13.
  * Time: 오전 10:39
  */
+var should = require('should');
 var oracle = require("oracle");
+var poolModule = require('generic-pool');
 
-var connectData = { "hostname": "192.168.1.39", "user": "aap4web", "password": "aap4web1234", "database": "orcl" };
-console.log('connecting : ', connectData);
-oracle.connect(connectData, function (err, connection) {
-    if (err) {
-        console.log('err : ', err);
-        return;
-    }
+describe('orcle test', function () {
+    var initData;
+    var pool;
 
-    console.log('conn : ', connection);
-
-
-    // selecting rows
-    connection.execute("SELECT * FROM serv_info", [], function (err, results) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(results);
-        }
-
-        connection.close(); // call this when you are done with the connection
+    before(function (done) {
+        initData = { "hostname": "192.168.1.39", "user": "aap4web", "password": "aap4web1234", "database": "orcl" };
+        pool = poolModule.Pool({
+            name: 'oracle',
+            // connection은 최대 10개까지 생성합니다.
+            max: 10,
+            // 생성된 connection은 30초 동안 유휴 상태(idle)면 destory됩니다.
+            idleTimeoutMillis: 30000,
+            log: true,
+            create: function (callback) {
+                oracle.connect(initData, function (err, conn) {
+                    callback(err, conn);
+                });
+            },
+            destroy: function (con) {
+                conn.close();
+            }
+        });
+        done();
     });
 
+
+    after(function (done) {
+        done();
+    });
+
+    it('oracle connection', function (done) {
+        this.timeout(15000);
+        var j = 0;
+        for (i = 0; i < 1000; i++) {
+            pool.acquire(function (err, conn) {
+                j++;
+                if (err) {
+                    console.log('err : ', err);
+                    return;
+                }
+                // selecting rows
+                conn.execute("SELECT * FROM serv_info", [], function (err, results) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log('result : ', results);
+                    }
+                    // return object back to pool
+                    pool.release(conn);
+
+                    console.log('j = ', j);
+                    if (j == 999) {
+                        done();
+                    }
+
+                });
+            });
+        }
+
+    });
 });
+
+
+
+
---
+++ test/templetTest.js
@@ -0,0 +1,22 @@
+/**
+ * User: nadir93
+ * Date: 13. 9. 13.
+ * Time: 오후 1:16
+ */
+var should = require('should');
+
+describe('describe test', function () {
+
+    before(function (done) {
+        done();
+    });
+
+
+    after(function (done) {
+        done();
+    });
+
+    it('test#1', function (done) {
+        done();
+    });
+});
