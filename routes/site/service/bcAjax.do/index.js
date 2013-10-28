4f5b44d99bcfe2b2f5e8d9e4c14c188b57618bad
nadir93 <nadir93@gmail.com>
2013-10-28 17:51:15 +0900

    bcAjax.do uri 추가

---
+++ routes/site/service/bcAjax.do/index.js
@@ -0,0 +1,26 @@
+/**
+ * User: nadir93
+ * Date: 13. 10. 28.
+ * Time: 오후 5:49
+ */
+var parser = require('cheerio'),
+    crypto = require('crypto'),
+    tidy = require('htmltidy').tidy,
+    logger = require('../../../logger');
+var srcName = __filename.substring(__filename.lastIndexOf('/'));
+
+exports.put = function (req, res, data, callback) {
+
+    try {
+        logger.debug(__filename + ' called put service/bcAjax.do/index.js');
+        //logger.debug(__filename + ' data :', data);
+        var $ = parser.load('<html>' + data + '</html>');
+        //req 에서 변경데이타를 뽑아
+        //virtual dom 에 적용하는 코드
+        //...
+        callback(null, $('html').html());
+    } catch (e) {
+        logger.error(e.stack);
+        callback(e);
+    }
+}
