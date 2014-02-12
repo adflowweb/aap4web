d5223c851db1662673ed890eb1fe475cdfabee90
nadir93 <nadir93@gmail.com>
2014-02-12 12:25:48 +0900

    추가 aaplus statis

---
+++ test/temp/modifyServerDom.js
@@ -0,0 +1,48 @@
+/**
+ * User: nadir93
+ * Date: 2014. 2. 12.
+ * Time: 오전 10:24
+ */
+var parser = require('cheerio');
+var fs = require('fs');
+
+//var rawHtml = fs.readFileSync('/Users/nadir93/Doc/Downloads/ASPN.html');
+var rawHtml = fs.readFileSync('/Users/nadir93/downloads/server.dom');
+//console.log('rawHtml : ', rawHtml.toString());
+var $ = parser.load(rawHtml);
+if ($('html').html()) {
+    $('meta').remove(); //remove meta tag
+    $('param').remove(); //remove param tag
+    $('link[rel="stylesheet"]').remove(); //remove param tag
+    $('*[style]').removeAttr('style'); //remove style attr
+    $('*[value]').removeAttr('value'); //remove value attr
+    $('*[type]').removeAttr('type'); //remove type attr
+    //$('*[selected]').removeAttr(); //remove selected attr
+    //$('img[@src$=.png']).removeAtrr('src');
+    //console.log("test : ",);
+    $('img[src$=".png"]').removeAttr('src');
+    console.log($('option[selected]'));
+    $('option[selected]').removeAttr('selected');
+    console.log($('option[selected]'));
+    $('option[selected]').removeAttr('selected');
+    $('form[name="searchForm"]').removeAttr('method');
+    $('link[href="/AAPlus/favicon.ico"]').remove();
+    $('input').removeAttr('type');
+    //$('*[src]').removeAttr('src'); //remove selected attr
+    //$('*[rel]').removeAttr('rel');
+    //console.log($('html').toArray());
+    //console.log($.html());
+    //console.log($('html').text().replace(/[\n\r]/g, '').replace(/\s+/g, ''));
+    //console.log($('html').text());
+
+
+    //var msg = $('html').html().replace(/style=\"[\w\#\'\(\)\-\.\,\/\:\;\_\s]*\"|value=\"\w+\"|type=\"[\w\/]+\"/g, '');
+    var msg = $('html').html();
+    //msg = msg.replace(/\<meta\scontent=[\w\"\#\(\)\-\.\,\/\:\;\_\s\=]*\/\>/g, '');
+    //msg = msg.replace(/\<param\s[\w\=\"\'\s\r\n\/\.\,]*\/\>/g, '');
+    console.log('msg : ', msg)
+
+}
+
+
+
